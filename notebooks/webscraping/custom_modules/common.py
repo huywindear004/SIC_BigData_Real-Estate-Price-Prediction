a89900ddb00ecc196d9e94eec5681c63091fa8a1
Nguyễn Quang Huy <huykhongcan04@gmail.com>
2024-07-14 14:44:36 +0700

    nothing

--- notebooks/webscraping/custom_modules/common.py
+++ notebooks/webscraping/custom_modules/common.py
@@ -51,7 +51,7 @@ def createChromeDriver() -> webdriver.Chrome:
         }
     }
     options.experimental_options["prefs"] = chrome_prefs
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
     )
